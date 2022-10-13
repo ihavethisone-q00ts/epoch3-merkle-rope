3b7d263bed6088885cfea55348f0fd88cde2990e
ihavethisone-q00ts <username@users.noreply.github.com>
2022-10-13 09:56:14 -0300

    Squashed commit of the following:
    commit 6ae97dc906708ddce38785ad1a135715282f5d33
    Author: ihavethisone-q00ts <username@users.noreply.github.com>
    Date:   Thu Oct 13 09:48:19 2022 -0300
    
        hideme

--- src/util/web3.ts
+++ src/util/web3.ts
@@ -1,13 +1,7 @@
 import { providers, Contract } from "ethers";
-// import { Alchemy, Network } from "alchemy-sdk";
 import merkleRopeAbi from "../abi/merkleRope.abi.json";
 
-// export const alchemy = new Alchemy({
-//   apiKey: "63-Ky27tDt4OWaRGfBrGUvHGzqXyI6Su",
-//   network: Network.ETH_GOERLI,
-// });
-
-const API_KEY = "63-Ky27tDt4OWaRGfBrGUvHGzqXyI6Su";
+const API_KEY = process.env.API_KEY;
 // export const MERKLE_ROPE = "0x346e2881d080a5d67d1f4229ebaf926772300650";
 export const MERKLE_ROPE = "0x55b36646b827cf1bbbeba7c5451a5c5f66862607";
 
