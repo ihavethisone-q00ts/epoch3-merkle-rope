0cdde3ed5567a1cfec24382215b1e8650898f316
ihavethisone-q00ts <username@users.noreply.github.com>
2022-10-13 10:19:27 -0300

    fix i18n

--- next.config.mjs
+++ next.config.mjs
@@ -17,8 +17,8 @@ export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 });
