098be0afd610db60ea4d9996bdea9de5f780be46
ihavethisone-q00ts <username@users.noreply.github.com>
2022-10-14 11:15:41 -0300

    fix ts error

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -1,2 +1,6 @@
-// @ts-ignore
-export const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// export const fetcher = <T>(...args: any[]) => fetch([...args]).then((res) => res.json());
+
+export async function fetcher<T>(request: RequestInfo): Promise<T> {
+  const response = await fetch(request);
+  return await response.json();
+}
