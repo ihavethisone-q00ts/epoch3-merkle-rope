5ec3ba7b135f0b46d223e5753fdef33395430070
ihavethisone-q00ts <username@users.noreply.github.com>
2022-10-14 10:16:37 -0300

    new fake contract

--- src/util/web3.ts
+++ src/util/web3.ts
@@ -3,7 +3,8 @@ import merkleRopeAbi from "../abi/merkleRope.abi.json";
 
 const API_KEY = process.env.API_KEY;
 // export const MERKLE_ROPE = "0x346e2881d080a5d67d1f4229ebaf926772300650";
-export const MERKLE_ROPE = "0x55b36646b827cf1bbbeba7c5451a5c5f66862607";
+// fake contract
+export const MERKLE_ROPE = "0xa8A25dCd3BF6C712563a99F8b15Be49628C4A3aF";
 
 export const provider = new providers.AlchemyProvider("goerli", API_KEY);
 
